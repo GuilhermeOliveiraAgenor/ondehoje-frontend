6c2d0891123c6955e0e239bfe002bc88e6c95b12
Guilherme <guilhermeagenor1012@gmail.com>
2026-03-23 21:08:16 -0300

    refactor: add mapbox_token in zod

--- src/env.ts
+++ src/env.ts
@@ -2,8 +2,10 @@ import { z } from 'zod'
 
 const envSchema = z.object({
   NEXT_PUBLIC_API_URL: z.url(),
+  NEXT_PUBLIC_MAPBOX_TOKEN: z.coerce.string(),
 })
 
 export const env = envSchema.parse({
   NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+  NEXT_PUBLIC_MAPBOX_TOKEN: z.coerce.string(),
 })
